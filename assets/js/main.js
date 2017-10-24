d37e1468be70619b6bfa3d9de5f3e2e1b49beef2
smartpigling <smartpigling@sina.com>
2017-10-24 17:25:47 +0800

    layout update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,14 @@
  * This file bundles all of your javascript together using webpack.
  */
 
+
 // JavaScript modules
 require('jquery');
 require('font-awesome-webpack');
 require('bootstrap');
 
+
 // Your own code
+// require('./app.js');
 require('./plugins.js');
 require('./script.js');
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
   entry: {
     main_js: './assets/js/main',
     main_css: [
-      path.join(__dirname, 'node_modules', 'font-awesome', 'css', 'font-awesome.css'),
-      path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css'),
+      // path.join(__dirname, 'node_modules', 'font-awesome', 'css', 'font-awesome.css'),
+      // path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css'),
       path.join(__dirname, 'assets', 'css', 'style.css'),
     ],
   },

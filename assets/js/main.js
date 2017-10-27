fd1bec2908bd7c38e2aed1ae62701cd4235434b7
smartpigling <smartpigling@sina.com>
2017-10-27 17:23:28 +0800

    20171027

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,14 @@
 require('jquery');
 require('font-awesome-webpack');
 require('bootstrap');
-require('fastclick/lib/fastclick');
-require('jquery-sparkline/jquery.sparkline');
-require('jvectormap/jquery-jvectormap');
-require('jquery-slimscroll/jquery.slimscroll');
-require('datatables.net/js/jquery.dataTables');
-require('datatables.net-bs/js/dataTables.bootstrap');
 require('admin-lte/dist/js/adminlte');
+// require('fastclick/lib/fastclick');
+// require('jquery-sparkline/jquery.sparkline');
+// require('jvectormap/jquery-jvectormap');
+// require('jquery-slimscroll/jquery.slimscroll');
+// require('datatables.net/js/jquery.dataTables');
+// require('datatables.net-bs/js/dataTables.bootstrap');
+// require('icheck/icheck');
 
 // Your own code
 require('./plugins.js');
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,2 @@
 // App initialization code goes here
 
-//Open active sidebar menu
-$(".sidebar-menu li[class='active']").parents("li").addClass("active menu-open");
-
-
-$("#demo").DataTable({
-    "ajax": {
-        "url": "/users/user_table/",
-        "dataSrc": "data"
-    },
-    "columns": [
-        {"data": "id"},
-        {"data": "username"},
-        {"data": "active"}
-    ]
-});
